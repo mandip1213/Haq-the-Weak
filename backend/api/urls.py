c403f55b84227547387c651fbeea9556116099c5
Aavash Chhetri <aavashchhetri01@gmail.com>
2022-01-25 14:14:25 +0545

    Registered the url router of tripping in the main api urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from django.urls import include,path
 
 from authentication.urls import router as user_router
+from tripping.urls import router as trip_router
 
 app_name = 'api'
 urlpatterns = [
@@ -12,5 +13,6 @@ urlpatterns = [
 
 router = DefaultRouter()
 router.registry.extend(user_router.registry)
+router.registry.extend(trip_router.registry)
 
 urlpatterns += router.urls
