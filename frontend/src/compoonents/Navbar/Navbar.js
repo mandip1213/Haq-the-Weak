b3e992cd7a5070abab9d270b9e7e757270a28e3c
Mandip thapa <thapamandip1213@gmail.com>
2022-01-27 17:16:40 +0545

    design for signup completed

--- frontend/src/compoonents/Navbar/Navbar.js
+++ frontend/src/compoonents/Navbar/Navbar.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<>
+			<Link to="/dashboard">dashboard</Link>
+
+
+		</>
+	)
 };
 
 export default Navbar;
