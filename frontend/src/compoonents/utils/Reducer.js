cd42a7bdced7474957d54db9a16bdb55f307f98e
Mandip thapa <thapamandip1213@gmail.com>
2022-01-29 23:52:49 +0545

    hooks for fetching data

--- frontend/src/compoonents/utils/Reducer.js
+++ frontend/src/compoonents/utils/Reducer.js
@@ -6,6 +6,10 @@ const Reducer = (state, { type, payload }) => {
 	}
 	if (type === "LOGOUT") {
 		return ({ ...state, access_token: "", refresh_token: "", uuid: "", isLoggedIn: false })
+	} if (type === "UPDATE_ACCESS") {
+		console.log("inside dispatch");
+		localStorage.setItem("access_token", JSON.stringify(payload.access_token))
+		return { ...state, access_token: payload.access_token }
 	}
 	return state;
 }
---
+++ frontend/src/compoonents/utils/UseFetch.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react"
+import useGlobalContext from "./Globalcontext";
+import URL from "../../baseurl"
+
+const useFetch = (endpoint) => {
+	const [state, setState] = useState({ isLoading: true, data: [], error: "" });
+	const { access_token, refresh_token, dispatch } = useGlobalContext();
+
+
+	useEffect(async () => {
+		try {
+			//try to fetch data
+			const _res = await fetch(`${URL}${endpoint}`, {
+				headers: { "Authorization": `Bearer ${access_token}` }
+			})
+
+			const res = await _res.json()
+			console.log("response from server : ", res);
+
+			if (_res.status === 401) {
+				//token exdpired now reresh the access token
+				console.log("token expired I guess")/* Todo verify properly */
+				const _res = await fetch(`${URL}/api/auth/token/refresh/`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({ refresh: refresh_token })
+				})
+				const res = await _res.json()
+				// console.log("refresh res ", res);
+				if (res.access) {
+					// console.log("before dispatch");
+					dispatch({ type: "UPDATE_ACCESS", payload: { access_token: res.access } })
+					// console.log("after dispatch");
+				} else {
+					setState({ ...state, error: "an unknown error occured ", isLoading: false })
+					//TODO: log user out
+				}
+				// console.log("after dispatch inside usefecth");
+
+				const ___res = await fetch(`${URL}${endpoint}`, {
+					headers: { "Authorization": `Bearer ${res.access}` }
+
+				});
+
+				const __res = await ___res.json()
+				if (___res.status !== 200) {
+					// console.log("error  inside ---res.status");
+					const error = res.detail ? res.detail : "AN unknown error occured"
+					setState({ ...state, error: error, isLoading: false })
+					return;
+				}
+
+				console.log("res from server  ", __res)
+				setState({ ...state, data: __res, isLoading: false })
+				return;
+
+			}
+
+			if (_res.status !== 200) {
+				console.log("error");
+				const error = res.detail ? res.detail : "AN unknown error occured"
+				setState({ ...state, error: error, isLoading: false })
+				return;
+			}
+			setState({ ...state, data: res, isLoading: false })
+
+		}
+
+		catch (error) {
+			console.log("error onreturn   ", error);
+			setState({ ...state, error: "Check all fields", isLoading: false })
+		}
+
+	}, []);
+
+	return state;
+
+
+}
+
+export default useFetch
