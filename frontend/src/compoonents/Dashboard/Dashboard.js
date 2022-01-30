0e6ef15b261ca0d3c6f2ec23f6e60099a2d1a98b
Mandip thapa <thapamandip1213@gmail.com>
2022-01-30 17:27:19 +0545

    data fecthing for Leaderboard page

--- frontend/src/compoonents/Dashboard/Dashboard.js
+++ frontend/src/compoonents/Dashboard/Dashboard.js
@@ -5,17 +5,80 @@ const Dashboard = () => {
 	const { isLoading, data: dashboard, error } = useFetch("/api/dashboard/");
 
 	// const { isLoading, data: dashboard, error } = useFetch("/api/visit/");
-	// console.log(dashboard);
+	//TODO return s all visits
 
+	console.log(dashboard);
+	if (isLoading) {
+		return (<div>loading</div>)
+	}
+	if (error) {
+		return (<div>error: {error}</div>)
+	}
 
+	const user = dashboard[0].user
+	const { first_name, last_name, username, profile_picture, batch, batch_count, total_score } = user;
+	// const { first_name, last_name, username, date_of_birth, gender, profile_picture, uuid, followers_count, following_count, followers, following, batch, batch_count } = user;
 
-	// if (isLoading) {
-	// 	return (<div>loading</div>)
-	// }
-	// if (error) {
-	// 	return (<div>error: {error}</div>)
-	// }
-	return <div>Dashboard</div>;
+	/*
+	*/
+	return (<div className="list">
+		<h1 className="head">Dashboard</h1>
+		<div className="list__body">
+
+			<div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+
+				<div className="left" style={{ display: "flex", alignItems: "center" }}>
+					<div className="name ">
+						<img className="user__image" /* tempclass */ src={profile_picture ? profile_picture : "https://www.formula1.com/content/fom-website/en/drivers/lewis-hamilton/_jcr_content/image.img.1920.medium.jpg/1533294345447.jpg"} alt="" /></div>
+					<div className="name"><h3>{username}</h3></div>
+				</div>
+
+				<div>
+					{/* TODO batches  */}
+					{/* TODO peronal links facebook social media */}
+					<h2>Total Reascore: 505</h2>
+				</div>
+
+			</div>
+			<table className='dashboard-table list__table'>
+				{/* note css for list* is imported from leaderboard.css */}
+
+				<thead className="dashboard-table-head">
+					<tr>
+						<th>SN</th>
+						<th>name</th>
+						<th>score</th>
+						<th>location</th>
+						<th>type of place</th>
+					</tr>
+				</thead>
+				<tbody>
+
+					{dashboard.map(({ user, vendor, id, score }, index) => {
+						let { name, location, image, type_of_place } = vendor
+						image = image ? image : "https://www.formula1.com/content/fom-website/en/drivers/lewis-hamilton/_jcr_content/image.img.1920.medium.jpg/1533294345447.jpg"
+
+						return (
+							<tr key={id} className='list__row'>
+								<td className="list__cell"><span className="list__value">{index + 1}</span></td>
+								<td className='list__cell with-image'>
+
+									<img className='user__image' src={image} alt="" />
+									<span className='list__value'>{name}</span>
+								</td>
+								<td className='list__cell'><span className='list__value'>{score.toFixed(2)}</span></td>
+								<td className='list__cell'><span className='list__value'>{location}</span></td>
+								<td className='list__cell'><span className='list__value'>{type_of_place}</span></td>
+							</tr>
+						)
+					})
+					}
+
+				</tbody>
+			</table>
+		</div >
+	</div >
+	)
 };
 
 export default Dashboard;
