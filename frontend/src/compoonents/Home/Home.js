ec8b59481ac7de7f4042b1a50086ab47a09287a8
Mandip thapa <thapamandip1213@gmail.com>
2022-02-03 00:07:15 +0545

    newsfeed ui done

--- frontend/src/compoonents/Home/Home.js
+++ frontend/src/compoonents/Home/Home.js
@@ -2,29 +2,66 @@ import React from 'react';
 import { Link } from "react-router-dom"
 import useGlobalContext from '../utils/Globalcontext';
 import LandingPage from "./LandingPage"
-import URL from "../../baseurl"
-import Sidebar from '../Sidebar/Sidebar';
-import Header from '../Header/Header';
-import MainContainer from '../MainContainer/MainContainer';
+import "./Home.css"
 import useFetch from '../utils/UseFetch';
+import Loading from "../extras/Loading"
 
 const Home = () => {
 	const { isLoggedIn, dispatch, access_token } = useGlobalContext()
-	useFetch("/api/feed/")
-	if (!isLoggedIn) {
-		return (
-			<LandingPage />
-		)
-	}
+	const { isLoading, data: feeds, error } = useFetch("/api/feed/")
+	if (isLoading) return <Loading />
+	if (error) return <div>error</div>;
+	if (feeds.length === 0) return <div>You have no feeds. Try following someone.</div>
 	return (
 		<>
+			<div className="activities">
+				{feeds.map(feed => {
+					feed.content = "Dont visit this place"
+					const { ratings, content, user: { username, gender, uuid, batch }, vendor: { name: vendorname, location, type_of_place } } = feed;
+					// TODO need date of visit
+					//TODO unique id
 
-			<MainContainer />
+					return (
+						<div className="activity">
 
+							<div className="activity__header">
+								<div className="activity__user">
+									<img className="activity__user-image" src="https://lh3.googleusercontent.com/ogw/ADea4I70PeEcDK4HGIqjH7bes64KQIL7TIq5lpPdBlIJJQ=s192-c-mo" />
+									<div>	<Link to={`/profile/${uuid}`}>{username}</Link> visited {location}.</div>
+								</div>
+								<span className="activity__date">10 minutes ago</span>
 
+							</div>
+							<div className="activity__body">
+
+								<div className="activity__body-main">
+									<div className="acitvity__vendor">
+										<span className="vendor-name">{vendorname}</span>
+										<span className='vendor-type'>{type_of_place}</span>
+									</div>
+									<div className="activity__stars"><Stars ratings={ratings} /></div>
+								</div>
+
+								{content && <div className='activity__content'>{content}</div>}
+							</div>
+						</div>
+					)
+				})}
+			</div>
 		</>
 	)
 };
+function Stars({ ratings }) {
 
+
+	return (<>
+		<span className={`activity__star ${ratings >= 1 && "rated"}`}>&#9733;</span>
+		<span className={`activity__star ${ratings >= 2 && "rated"}`}>&#9733;</span>
+		<span className={`activity__star ${ratings >= 3 && "rated"}`}>&#9733;</span>
+		<span className={`activity__star ${ratings >= 4 && "rated"}`}>&#9733;</span>
+		<span className={`activity__star ${ratings >= 5 && "rated"}`}>&#9733;</span>
+	</>
+	)
+}
 
 export default Home;
